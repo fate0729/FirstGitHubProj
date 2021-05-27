42eafb2b4c089ec268cb34f54528e9cbcbfe57d1
unknown <fate0729@163.com>
2021-05-27 15:38:11 +0800

    测试

--- SimpleProj/src/Test3.java
+++ SimpleProj/src/Test3.java
@@ -1,5 +1,9 @@
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Test3");
+        int a = 0;
+        int b = 1;
+        int c = a +b;
+        System.out.println(c);
     }
 }
